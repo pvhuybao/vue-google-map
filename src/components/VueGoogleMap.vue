320ed49939895ed1514aaa7ce4f7bca9e5347841
pvhuybao <baopham@okxe.vn>
2022-02-10 16:27:12 +0700

    first commit

---
+++ src/components/VueGoogleMap.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+		{{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "vue-google-map",
+  props: {
+		msg: {
+			type: String,
+			default: 'asd'
+		}
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
